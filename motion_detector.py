26845f212a3e2e61f21635f45fc4c52a8f594d17
Arecio Canton <cuba289@gmail.com>
2021-11-07 04:55:48 -0500

    Added delta frmae

--- motion_detector.py
+++ motion_detector.py
@@ -1,4 +1,7 @@
 import cv2
+
+first_frame = None
+
 # video obj with method for capturing video (0,1,2 = webcam or path to video)
 # Trigger camera
 video=cv2.VideoCapture(0)
@@ -11,17 +14,25 @@ while True:
     # Reads 1st Frame
     check, frame = video.read()
 
-    print(check)
-    print(frame)
-
-
     # Converts frame to Grayscale
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    # make img blurry to remove noise (width,height of Gaussian kernel), standard deviation
+    gray=cv2.GaussianBlur(gray,(21,21),0)
+
+    if first_frame is None:
+        # sets first frame = to gray frame being rendered in loop
+        first_frame = gray
+        # makes python go to the beginning of the loop
+        continue
+    # compare background img with current frame / gives another img
+    delta_frame=cv2.absdiff(first_frame,gray)
 
     # Created window and showed 1st frame of video
-    cv2.imshow("Capturing",gray)
+    cv2.imshow("Gray Frame",gray)
+    cv2.imshow("Delta Frame",delta_frame)
     # Tells the script to wait for 1ms
     key=cv2.waitKey(1)
+    print(gray)
     # if you press Q key it stops the loop
     if key == ord('q'):
         break
