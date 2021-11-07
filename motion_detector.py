01789824466d2209ac345a831040b558380dd7f6
Arecio Canton <cuba289@gmail.com>
2021-11-07 05:22:08 -0500

    added threshold frame

--- motion_detector.py
+++ motion_detector.py
@@ -9,15 +9,13 @@ video=cv2.VideoCapture(0)
 
 # While Loop through Frame array and use imshow to show each frame in the array
 while True:
-    # Frame obj for showing window which reads video obj
-    # check = bool / frame = 3D numpy array which rep img (first frame vid captures)
     # Reads 1st Frame
     check, frame = video.read()
 
     # Converts frame to Grayscale
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     # make img blurry to remove noise (width,height of Gaussian kernel), standard deviation
-    gray=cv2.GaussianBlur(gray,(21,21),0)
+    gray=cv2.GaussianBlur(gray,(21,21), 0)
 
     if first_frame is None:
         # sets first frame = to gray frame being rendered in loop
@@ -26,14 +24,24 @@ while True:
         continue
     # compare background img with current frame / gives another img
     delta_frame=cv2.absdiff(first_frame,gray)
+    # if pixel is less then 30 gets changed to white, Threshold Method returns a tuple we need 2nd item which is the frame
+    thresh_frame=cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    # Smooth threshold frame
+    thresh_frame=cv2.dilate(thresh_frame, None, iterations=2)
+    # Find Contours of thresh copy and store in tuple, draw extranal obj that will be finding in img, approx method CV applies for retrieving contours 
+    (cnts,_) = cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
 
     # Created window and showed 1st frame of video
     cv2.imshow("Gray Frame",gray)
     cv2.imshow("Delta Frame",delta_frame)
+    cv2.imshow("Threshold Frame",thresh_frame)
     # Tells the script to wait for 1ms
     key=cv2.waitKey(1)
     print(gray)
-    # if you press Q key it stops the loop
+    print(delta_frame)
+    print(thresh_frame)
+    # if you press Q you quit
     if key == ord('q'):
         break
 
