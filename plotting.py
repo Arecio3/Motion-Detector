ab510b473b5ec070f2ccb3fdacbe0c9b233e80d5
Arecio Canton <cuba289@gmail.com>
2021-11-09 17:35:17 -0500

    we gotta chart !" " "

--- plotting.py
+++ plotting.py
@@ -1,2 +1,12 @@
 # grab dataframe thats being generated in other file
 from motion_detector import df
+from bokeh.plotting import figure, show, output_file
+# creating chart figure container
+p=figure(x_axis_type='datetime',height=300,width=1000,title='Motion Detection Graph')
+# left border = start time
+# right border = end time
+# top = fixed
+# bottom = fixed
+q=p.quad(left=df['Start'],right=df['End'],bottom=0,top=1, color='red')
+output_file('index.html')
+show(p)
